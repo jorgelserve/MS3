08ece8cf9358759cba3743cdbbdebfe2c2a9671e
Federico Agudelo <fagudel7@eafit.edu.co>
2018-04-22 00:00:22 -0500

    pagina webbbbbbbb con ssdv

--- AplicacionWeb/DiegoAngular/src/app/Services/location.service.ts
+++ AplicacionWeb/DiegoAngular/src/app/Services/location.service.ts
@@ -6,9 +6,11 @@ import { isNull } from 'lodash';
 @Injectable()
 export class LocationService {
   public url: string;
+  public url1: string;
 
   constructor(private _http: Http) {
     this.url='http://www.cansats3kratos.me/data/'
+    this.url1='http://www.cansats3kratos.me/images/'
   }
 
 
@@ -21,5 +23,9 @@ export class LocationService {
   getLocation(){
     return this._http.get('https://maps.googleapis.com/maps/api/geocode/json?address=medellin')
                       .map(res => res.json());
+  }
+
+  getImage(){
+    return this._http.get(this.url1).map(res=>res.json());
   }
 }
